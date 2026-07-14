33b9a35f3d3b410ce26a0ecfef9aca12ac289e72
zhangchao <1184569302@qq.com>
2026-07-14 20:40:10 +0800

    fix: restore column save action in workbench

--- test/workspace-template.test.js
+++ test/workspace-template.test.js
@@ -6,7 +6,7 @@ const { createWorkspaceMarkup } = require("../workspace-template");
 test("provides loading progress, print fit summary and physical-page preview root", () => {
   const markup = createWorkspaceMarkup();
 
-  ["groupSelect", "monthPickerButton", "monthPickerDialog", "monthGrid", "queryButton", "paperSelect", "printButton", "cancelLoadButton", "progressBar", "loadProgress", "printSummary", "printPages", "columnEditor"].forEach((id) => {
+  ["groupSelect", "monthPickerButton", "monthPickerDialog", "monthGrid", "queryButton", "paperSelect", "printButton", "saveColumns", "cancelLoadButton", "progressBar", "loadProgress", "printSummary", "printPages", "columnEditor"].forEach((id) => {
     assert.match(markup, new RegExp('id="' + id + '"'));
   });
 });
