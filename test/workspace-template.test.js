b3fce360576d1f27531861d4efcebbf6bca2e22a
zhangchao <1184569302@qq.com>
2026-07-15 21:12:35 +0800

    chore: simplify salary print query header

--- test/workspace-template.test.js
+++ test/workspace-template.test.js
@@ -10,6 +10,8 @@ test("provides loading progress, print fit summary and physical-page preview roo
     assert.match(markup, new RegExp('id="' + id + '"'));
   });
   assert.match(markup, /<option value="B4 landscape">B4 横向<\/option>/);
+  assert.match(markup, /id="queryButton" class="primary">查询<\/button>/);
   assert.match(markup, /字段顺序与显示请在左侧“打印列”调整。/);
   assert.match(markup, /class="column-panel[^>]*>[\s\S]*id="columnEditor"/);
+  assert.doesNotMatch(markup, /masthead reveal/);
 });
